faabc6ef64b7bd54ca385f0aa0093d1ee071f6b4
Jeferson Luis <jeferson.luis@g.globo>
2024-08-22 22:30:02 -0300

    a

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Importa as dependências necessárias
-const express = require('express');
-const cron = require('node-cron');
-const axios = require('axios');
+const express = require("express");
+const cron = require("node-cron");
+const axios = require("axios");
 
-require('dotenv').config();
+require("dotenv").config();
 
 // Configura o aplicativo Express
 const app = express();
@@ -14,26 +14,35 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 // Rota simples para a raiz
-app.get('/', (req, res) => {
-    res.send('Servidor Node.js está rodando!');
-  });
+app.get("/", (req, res) => {
+  res.send("Servidor Node.js está rodando!");
+});
 
-app.get('/test', (req, res) => {
-    res.send('Servidor Node.js está rodando!');
+app.get("/test", (req, res) => {
+  res.send("Servidor Node.js está rodando!");
 });
 
 // Configura um cron job para ser executado a cada minuto (você pode ajustar a expressão cron)
-cron.schedule('*/5 * * * * *', async () => {
+cron.schedule(
+  "*/5 * * * * *",
+  async () => {
     try {
-        // Faz uma requisição GET a uma URL de exemplo (substitua pela URL desejada)
-        const response = await axios.get('https://trabalho-faculdade-1.onrender.com/work');
-        // Processa a resposta
-        console.log('Dados recebidos:', response.data);
-      } catch (error) {
-        // Captura e exibe qualquer erro ocorrido durante a requisição
-        console.error('Erro na requisição GET:', error.message);
-      }
-});
+      // Faz uma requisição GET a uma URL de exemplo (substitua pela URL desejada)
+      const response = await axios.get(
+        "https://trabalho-faculdade-1.onrender.com/work"
+      );
+      // Processa a resposta
+      console.log("Dados recebidos:", response.data);
+    } catch (error) {
+      // Captura e exibe qualquer erro ocorrido durante a requisição
+      console.error("Erro na requisição GET:", error.message);
+    }
+  },
+  {
+    scheduled: true,
+    timezone: "America/Sao_Paulo",
+  }
+);
 
 // Inicia o servidor
 app.listen(PORT, () => {
